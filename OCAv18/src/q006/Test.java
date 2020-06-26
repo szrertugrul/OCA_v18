bfb2151db4e9a217ee247f941fcbf3e01b951d0c
sezerertugrul <sezerertugrul@sezers-mac-mini>
2020-06-26 03:08:07 -0400

    a

--- OCAv18/src/q006/Test.java
+++ OCAv18/src/q006/Test.java
@@ -7,7 +7,7 @@ class X{
 			throw new IOException();
 		}
 	}
- class Test {
+public class Test {
 	public static void main(String[] args) throws Exception {
 		X xobj = new X();
 		xobj.printFileContent();		
